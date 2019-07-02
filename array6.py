33d00c4b9c946cb630585a2672cc49aa41270e84
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-07-02 15:45:26 +0530

    Create array6.py

---
+++ array6.py
@@ -0,0 +1,15 @@
+   sv=int(input())
+   l=list(map(int,input().split()))
+   flag=1
+   l1=[]
+ for x in range (len(l)):
+      for i in range(x+1,len(l)):
+          if(l[x]<l[i]):
+              flag=0
+               break
+         else:
+             flag=1
+      if (flag==1):
+         l1.append(l[x])
+ print(*l1)
+ print(max(l))
